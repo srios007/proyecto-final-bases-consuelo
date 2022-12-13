32d45a1fd7bf2023a19dc43744415bd21823a5fa
Christian <chcarov@correo.udistrital.edu.co>
2022-12-13 17:27:11 -0500

    Errores generales corregidos. Implementación de llaves primarias automáticas a algunos procedimientos.

--- PLSQL/Procedures/Procedures.sql
+++ PLSQL/Procedures/Procedures.sql
@@ -40,7 +40,6 @@ BEGIN
             DUAL;
         FOR R_SALDOS IN (
             SELECT
-                CC.SALDO_ACTUAL,
                 C.COD_CONJUNTO,
                 C.VALOR_TASA_MORA,
                 C.VALOR_TASA_DESCUENTO,
@@ -48,6 +47,8 @@ BEGIN
                 A.COD_BLOQUE,
                 A.COD_APARTAMENTO,
                 CC.COD_CUENTA_COBRO,
+                CC.SALDO_ACTUAL,
+                CC.SALDO_PENDIENTE,
                 CC.VALOR_DESCUENTO,
                 CC.VALOR_MORA,
                 CC.PERIODO_MES_CUENTA,
@@ -78,10 +79,9 @@ BEGIN
                         ESTADO_CUENTA = 'En mora'
                     WHERE
                         COD_CUENTA_COBRO = R_SALDOS.COD_CUENTA_COBRO;
-                    PR_SALDO_PENDIENTE(R_SALDOS.COD_CONJUNTO, R_SALDOS.COD_BLOQUE, R_SALDOS.COD_APARTAMENTO, R_PERIODO.PERIODO_MES_CUENTA, R_PERIODO.PERIODO_ANIO_CUENTA);
                 END IF;
                 COMMIT;
-            ELSIF R_SALDOS.PERIODO_MES_CUENTA < LN_MES_ACTUAL OR R_SALDOS.PERIODO_ANIO_CUENTA < LN_ANIO_ACTUAL THEN
+            ELSIF R_SALDOS.PERIODO_MES_CUENTA < LN_MES_ACTUAL AND R_SALDOS.PERIODO_ANIO_CUENTA <= LN_ANIO_ACTUAL THEN
                 IF R_SALDOS.VALOR_MORA = 0 THEN
                     IF R_SALDOS.VALOR_DESCUENTO > 0 THEN
                         R_SALDOS.SALDO_ACTUAL := R_SALDOS.SALDO_ACTUAL + ( R_SALDOS.SALDO_ACTUAL * (R_SALDOS.VALOR_TASA_DESCUENTO / 100));
@@ -95,7 +95,6 @@ BEGIN
                         ESTADO_CUENTA = 'En mora'
                     WHERE
                         COD_CUENTA_COBRO = R_SALDOS.COD_CUENTA_COBRO;
-                    PR_SALDO_PENDIENTE(R_SALDOS.COD_CONJUNTO, R_SALDOS.COD_BLOQUE, R_SALDOS.COD_APARTAMENTO, R_PERIODO.PERIODO_MES_CUENTA, R_PERIODO.PERIODO_ANIO_CUENTA);
                 END IF;
                 COMMIT;
             ELSIF R_SALDOS.DIA_OPORTUNO >= LN_DIA_ACTUAL AND R_SALDOS.PERIODO_MES_CUENTA = LN_MES_ACTUAL AND R_SALDOS.PERIODO_ANIO_CUENTA = LN_ANIO_ACTUAL THEN
@@ -109,7 +108,6 @@ BEGIN
                         ESTADO_CUENTA = 'Pendiente'
                     WHERE
                         COD_CUENTA_COBRO = R_SALDOS.COD_CUENTA_COBRO;
-                    PR_SALDO_PENDIENTE(R_SALDOS.COD_CONJUNTO, R_SALDOS.COD_BLOQUE, R_SALDOS.COD_APARTAMENTO, R_PERIODO.PERIODO_MES_CUENTA, R_PERIODO.PERIODO_ANIO_CUENTA);
                 END IF;
                 COMMIT;
             ELSIF R_SALDOS.PERIODO_MES_CUENTA > LN_MES_ACTUAL OR R_SALDOS.PERIODO_ANIO_CUENTA > LN_ANIO_ACTUAL THEN
@@ -123,7 +121,6 @@ BEGIN
                         ESTADO_CUENTA = 'Pendiente'
                     WHERE
                         COD_CUENTA_COBRO = R_SALDOS.COD_CUENTA_COBRO;
-                    PR_SALDO_PENDIENTE(R_SALDOS.COD_CONJUNTO, R_SALDOS.COD_BLOQUE, R_SALDOS.COD_APARTAMENTO, R_PERIODO.PERIODO_MES_CUENTA, R_PERIODO.PERIODO_ANIO_CUENTA);
                 END IF;
                 COMMIT;
             ELSE
@@ -131,6 +128,17 @@ BEGIN
                 ROLLBACK;
                 RAISE LE_INCONSISTENCIA;
             END IF;
+ -- IF R_SALDOS.SALDO_ACTUAL = 0 AND R_SALDOS.SALDO_PENDIENTE = 0 THEN
+ --     UPDATE CUENTA_COBRO
+ --     SET
+ --         VALOR_MORA = 0,
+ --         VALOR_DESCUENTO = 0,
+ --         ESTADO_CUENTA = 'Pagado'
+ --     WHERE
+ --         COD_CUENTA_COBRO = R_SALDOS.COD_CUENTA_COBRO;
+ --     COMMIT;
+ -- END IF;
+            PR_SALDO_PENDIENTE(R_SALDOS.COD_CONJUNTO, R_SALDOS.COD_BLOQUE, R_SALDOS.COD_APARTAMENTO, R_PERIODO.PERIODO_MES_CUENTA, R_PERIODO.PERIODO_ANIO_CUENTA);
         END LOOP;
     END LOOP;
 EXCEPTION
@@ -233,8 +241,6 @@ END;
 
 ------------------------------------------------------ Procedimiento para pagar una cuenta de cobro
 CREATE OR REPLACE PROCEDURE PR_PAGAR_CUENTA (
-    PK_PAGO IN PAGO.COD_PAGO%TYPE,
-    PK_CUENTA IN PAGO.COD_CUENTA_COBRO%TYPE,
     PK_APTO IN PAGO.COD_APARTAMENTO%TYPE,
     PK_BLOQUE IN PAGO.COD_BLOQUE%TYPE,
     PK_CONJUNTO IN PAGO.COD_CONJUNTO%TYPE,
@@ -243,7 +249,36 @@ CREATE OR REPLACE PROCEDURE PR_PAGAR_CUENTA (
     PC_ERROR OUT INTEGER,
     PM_ERROR OUT VARCHAR
 ) AS
+    LK_PAGO   PAGO.COD_PAGO%TYPE;
+    LK_CUENTA PAGO.COD_CUENTA_COBRO%TYPE;
 BEGIN
+    SELECT
+        MAX(COD_CUENTA_COBRO) INTO LK_CUENTA
+    FROM
+        CUENTA_COBRO
+    WHERE
+        COD_CONJUNTO = PK_CONJUNTO
+        AND COD_BLOQUE = PK_BLOQUE
+        AND COD_APARTAMENTO = PK_APTO
+    GROUP BY
+        SALDO_PENDIENTE
+    HAVING
+        SALDO_PENDIENTE = 0;
+    IF LK_CUENTA IS NULL THEN
+        LK_CUENTA := 1;
+        LK_PAGO := 1;
+    ELSE
+        SELECT
+            MAX(COD_PAGO) INTO LK_PAGO
+        FROM
+            PAGO
+        WHERE
+            COD_CUENTA_COBRO = LK_CUENTA;
+        LK_PAGO := LK_PAGO + 1;
+        IF LK_PAGO IS NULL THEN
+            LK_PAGO := 1;
+        END IF;
+    END IF;
     INSERT INTO PAGO (
         COD_PAGO,
         COD_CUENTA_COBRO,
@@ -254,8 +289,8 @@ BEGIN
         FECHA_PAGO,
         FORMA_PAGO
     ) VALUES (
-        PK_PAGO,
-        PK_PAGO,
+        LK_PAGO,
+        LK_CUENTA,
         PK_APTO,
         PK_BLOQUE,
         PK_CONJUNTO,
@@ -263,6 +298,7 @@ BEGIN
         (SELECT CURRENT_TIMESTAMP FROM DUAL),
         PT_PAGO
     );
+    PR_CALC_DESC_MORA(PC_ERROR, PM_ERROR);
     COMMIT;
 EXCEPTION
     WHEN OTHERS THEN
@@ -272,6 +308,20 @@ EXCEPTION
             || SQLCODE
             || SQLERRM);
 END PR_PAGAR_CUENTA;
+/
+
+------------------------------------------------------ Bloque anónimo para llamar al procedimiento PR_PAGAR_CUENTA
+DECLARE
+    K_APTO     PAGO.COD_APARTAMENTO%TYPE := 402;
+    K_BLOQUE   PAGO.COD_BLOQUE%TYPE := 11;
+    K_CONJUNTO PAGO.COD_CONJUNTO%TYPE := 1;
+    V_PAGO     PAGO.VALOR_PAGADO%TYPE := 100000;
+    T_PAGO     PAGO.FORMA_PAGO%TYPE := 'Débito';
+    C_ERROR    INTEGER;
+    M_ERROR    VARCHAR(150);
+BEGIN
+    PR_PAGAR_CUENTA(K_APTO, K_BLOQUE, K_CONJUNTO, V_PAGO, T_PAGO, C_ERROR, M_ERROR);
+END;
 /
 
 ------------------------------------------------------ Procedimiento para reservar una zona común
@@ -349,7 +399,7 @@ EXCEPTION
 END PR_CREAR_RESERVA;
 /
 
------------------------------------------------------- Procedimiento para generar la cuenta de cobro de un apartamento dado
+------------------------------------------------------ Procedimiento para crear la cuenta de cobro de un apartamento dado
 CREATE OR REPLACE PROCEDURE PR_CREAR_CUENTA_COBRO (
     PK_APTO IN CUENTA_COBRO.COD_APARTAMENTO%TYPE,
     PK_BLOQUE IN CUENTA_COBRO.COD_BLOQUE%TYPE,
@@ -401,11 +451,26 @@ BEGIN
 EXCEPTION
     WHEN OTHERS THEN
         PC_ERROR := 1;
-        PM_ERROR := 'PR_CREAR_CUENTA_COBRO "OTHERS" raised';
+        PM_ERROR := 'Ha ocurrido un error al crear la cuenta de cobro número: '
+            || LK_CUENTA;
         RAISE_APPLICATION_ERROR(-20001, 'PR_CREAR_CUENTA_COBRO Ha ocurrido un error: '
             || SQLCODE
             || SQLERRM);
 END PR_CREAR_CUENTA_COBRO;
+/
+
+------------------------------------------------------ Bloque anónimo para llamar al procedimiento PR_CREAR_CUENTA_COBRO
+DECLARE
+    K_APTO     CUENTA_COBRO.COD_APARTAMENTO%TYPE := 402;
+    K_BLOQUE   CUENTA_COBRO.COD_BLOQUE%TYPE := 11;
+    K_CONJUNTO CUENTA_COBRO.COD_CONJUNTO%TYPE := 1;
+    N_MES      CUENTA_COBRO.PERIODO_MES_CUENTA%TYPE := 9;
+    N_ANIO     CUENTA_COBRO.PERIODO_ANIO_CUENTA%TYPE := 2022;
+    C_ERROR    INTEGER;
+    M_ERROR    VARCHAR(150);
+BEGIN
+    PR_CREAR_CUENTA_COBRO(K_APTO, K_BLOQUE, K_CONJUNTO, N_MES, N_ANIO, C_ERROR, M_ERROR);
+END;
 /
 
 ------------------------------------------------------ Procedimiento para pagar la cuenta de cobro (con saldo > 0) más antigua.
@@ -544,7 +609,7 @@ BEGIN
     END IF;
 EXCEPTION
     WHEN OTHERS THEN
-        RAISE_APPLICATION_ERROR(-20001, 'PR_CALC_SALDOS Ha ocurrido un error: '
+        RAISE_APPLICATION_ERROR(-20001, 'PR_PAGAR_SALDO Ha ocurrido un error: '
             || SQLCODE
             || SQLERRM);
 END PR_PAGAR_SALDO;
@@ -639,6 +704,11 @@ BEGIN
             LN_ANIO := LN_ANIO + 1;
         END IF;
     END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20001, 'PR_SALDO_PENDIENTE Ha ocurrido un error: '
+            || SQLCODE
+            || SQLERRM);
 END PR_SALDO_PENDIENTE;
 /
 
@@ -696,5 +766,10 @@ BEGIN
             END IF;
         END IF;
     END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR(-20001, 'PR_INIT_SALDOS Ha ocurrido un error: '
+            || SQLCODE
+            || SQLERRM);
 END PR_INIT_SALDOS;
 /
